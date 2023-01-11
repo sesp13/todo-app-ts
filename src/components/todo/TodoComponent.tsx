804c67fffdcbcf7f486b60717390f999ca710100
sesp13 <sespinosarte99@gmail.com>
2023-01-10 23:40:04 -0500

    Add delete and toggle actions in the store

--- src/components/todo/TodoComponent.tsx
+++ src/components/todo/TodoComponent.tsx
@@ -1,16 +1,34 @@
+import { useDispatch } from 'react-redux';
+import { deleteTodo, toggleTodo } from '../../store';
 import './TodoComponent.scss';
 import { TodoComponentProps } from './TodoComponentProps';
 
 export const TodoComponent = ({ todoItem }: TodoComponentProps) => {
+  const dispatch = useDispatch();
+
+  const onToggleTodo = () => {
+    const action = toggleTodo(todoItem.id);
+    dispatch(action);
+  };
+
+  const onDeleteTodo = () => {
+    const action = deleteTodo(todoItem.id);
+    dispatch(action);
+  };
+
   return (
     <div className="row todo-box">
       <div className="col-6">
         <p>{todoItem.text}</p>
       </div>
       <div className="col-6 buttons-box">
-        <button className="btn btn-primary">Done</button>
+        <button className="btn btn-primary" onClick={() => onToggleTodo()}>
+          {todoItem.done ? 'Uncomplete' : 'Done'}
+        </button>
         <button className="btn btn-secondary">Edit</button>
-        <button className="btn btn-danger">Delete</button>
+        <button className="btn btn-danger" onClick={() => onDeleteTodo()}>
+          Delete
+        </button>
       </div>
     </div>
   );
--- src/store/slices/todo/todoSlice.ts
+++ src/store/slices/todo/todoSlice.ts
@@ -16,17 +16,37 @@ const getInitialState = (): TodoState => {
   };
 };
 
+const saveTodos = (todos: Todo[]) => {
+  // Question: Where Should I implement the saving in the localstorage
+  const newTodos = JSON.stringify(todos);
+  localStorage.setItem('todos', newTodos);
+};
+
 export const todoSlice = createSlice({
   name: 'todo',
   initialState: getInitialState(),
   reducers: {
     addTodo: (state: TodoState, action: PayloadAction<Todo>) => {
       state.todos.unshift(action.payload);
-      // Question: Where Should I implement the saving in the localstorage
-      const newTodos = JSON.stringify(state.todos);
-      localStorage.setItem('todos', newTodos);
+      saveTodos(state.todos);
+    },
+    toggleTodo: (state: TodoState, action: PayloadAction<number>) => {
+      const id = action.payload;
+      state.todos = state.todos.map((todo: Todo) => {
+        if (todo.id === id) {
+          return { ...todo, done: !todo.done };
+        } else {
+          return todo;
+        }
+      });
+      saveTodos(state.todos);
+    },
+    deleteTodo: (state: TodoState, action: PayloadAction<number>) => {
+      const id = action.payload;
+      state.todos = state.todos.filter((todo) => todo.id !== id);
+      saveTodos(state.todos);
     },
   },
 });
 
-export const { addTodo } = todoSlice.actions;
+export const { addTodo, toggleTodo, deleteTodo } = todoSlice.actions;
